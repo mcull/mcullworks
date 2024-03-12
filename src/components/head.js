92f4bb2b0a557943dd68e63d87061c85831e92da
mcull <marc.cull@gmail.com>
2024-03-12 11:49:49 -0700

    add accessibilty to head

--- src/components/head.js
+++ src/components/head.js
@@ -18,6 +18,10 @@ export default function Head({ title, description, image }) {
       <meta name="twitter:title" content={title} />
       {description && <meta name="twitter:description" content={description} />}
       {image && <meta name="twitter:image" content={image.url} />}
+      <script type="application/ld+json" id="oada_ma_toolbar_script"> {`
+        var oada_ma_license_key="SenJyTE127LcFedXEhORoKJ3Q4cGw4NTmEfthYdh0VxY1BME64";var oada_ma_license_url="https://api.maxaccess.io/scripts/toolbar/SenJyTE127LcFedXEhORoKJ3Q4cGw4NTmEfthYdh0VxY1BME64";(function(s,o,g){a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.src=g;a.setAttribute("async","");a.setAttribute("type","text/javascript");a.setAttribute("crossorigin","anonymous");m.parentNode.insertBefore(a,m)})(document,"script",oada_ma_license_url+oada_ma_license_key)
+        `}
+      </script>
     </>
   )
 }
